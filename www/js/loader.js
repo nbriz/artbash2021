158c06786eec54a1cb4f25e5ba95a7ca925e76e3
nbriz <nickbriz@gmail.com>
2021-04-26 08:44:47 -0500

    safari warning

--- www/js/loader.js
+++ www/js/loader.js
@@ -1,6 +1,13 @@
+/* global Averigua */
 window.createLoader = function (callbak, hideInfo) {
   const loader = document.createElement('loader')
   loader.className = 'loader'
+  if (Averigua.browserInfo().name === 'Safari') {
+    const wrn = document.createElement('div')
+    wrn.className = 'instructions'
+    wrn.innerHTML = 'It appears you\'re using Safari, this browser does not have great support for WebGL and may not properly render all the elements in the gallery. Try using a modern browser like <a href="https://brave.com/" target="_blank">Brave</a>, <a href="https://www.mozilla.org/en-US/firefox/new/" target="_blank">Firefox</a> or <a href="https://www.google.com/chrome/" target="_blank">Chrome</a>.'
+    loader.appendChild(wrn)
+  }
   const btn = document.createElement('div')
   btn.className = 'btn'
   loader.appendChild(btn)
